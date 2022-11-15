14f0acba5e4c96eda56b22d15f2d186f38ed0a32
agryz01 <agryz@yandex.ru>
2022-11-15 15:00:38 +0300

    PR15-02-02-01

--- backend/app.js
+++ backend/app.js
@@ -20,15 +20,15 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
-const allowedCors = [
-  'localhost:3000'
-];
+// const allowedCors = [
+//   'localhost:3000'
+// ];
 
 app.use(function(req, res, next) {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // const { origin } = req.headers;
+  // if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', "*");
+  // }
   next();
 });
 
