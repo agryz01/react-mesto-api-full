9b21ed11fd925574e0922a06625f9f917f0d1c49
agryz01 <agryz@yandex.ru>
2022-11-15 17:02:04 +0300

    PR15-02-02-01

--- backend/app.js
+++ backend/app.js
@@ -20,17 +20,24 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
-// const allowedCors = [
-//   'localhost:3000'
-// ];
+const allowedCors = [
+  'localhost:3000'
+];
 
-app.use(function(req, res, next) {
-  // const { origin } = req.headers;
-  // if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', "*");
-  // }
+app.use(function (req, res, next) {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   next();
 });
+
+
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
